40e663078c589aca80465e3f428d580475477400
Alexander Bogomolov <obogom@softserveinc.com>
2021-08-06 16:36:38 +0300

    add batch_size parameter

--- train.py
+++ train.py
@@ -35,6 +35,7 @@ def main(args=None):
 
     parser.add_argument('--model', help='Model weights')
     parser.add_argument('--starting_epoch', help='Starting epoch', type=int, default=0)
+    parser.add_argument('--batch_size', help='Batch Size', type=int, default=4)
 
     parser = parser.parse_args(args)
 
@@ -70,7 +71,7 @@ def main(args=None):
     else:
         raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
 
-    sampler = AspectRatioBasedSampler(dataset_train, batch_size=4, drop_last=False)
+    sampler = AspectRatioBasedSampler(dataset_train, batch_size=parser.batch_size, drop_last=False)
     dataloader_train = DataLoader(dataset_train, num_workers=3, collate_fn=collater, batch_sampler=sampler)
 
     if dataset_val is not None:
